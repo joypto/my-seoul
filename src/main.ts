2bdff79cfb4a7d628e5424c6f00355612af1f4e0
joypto <sooie35@gmail.com>
2023-01-20 21:29:40 +0900

    Feat set swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
+import { INestApplication } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { SwaggerTheme } from 'swagger-themes';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+const swagger = (app: INestApplication) => {
+  const config = new DocumentBuilder()
+    .setTitle('MY SEOUL API')
+    .setDescription('API description used in My Seoul')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  const theme = new SwaggerTheme('v3');
+  const options = {explorer: true, customeCss: theme.getBuffer('dark')};
+  SwaggerModule.setup('api', app, document, options);
 }
+
+const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+
+  swagger(app);
+
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT')
+  await app.listen(port);
+}
+
 bootstrap();
